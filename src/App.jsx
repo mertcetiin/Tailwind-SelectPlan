79de5f84544881a52b003bce4e31d004d03e34de
mertcetiin <mrtctn506@gmail.com>
2023-09-29 01:31:50 +0300

    monthlyPlan made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { useState } from 'react';
+import { monthlyPlan } from './components/Plan';
 
 function App() {
 
@@ -8,6 +9,10 @@ function App() {
   const handleSelect = (planType) => {
     setPlanSelected(planType)
   }
+
+  const [plan, setPlan] = useState(monthlyPlan);
+
+
 
   return (
     <section className="text-gray-600 body-font overflow-hidden">
@@ -24,7 +29,7 @@ function App() {
           <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
             <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
               <h2 className="text-sm tracking-widest title-font mb-1 font-medium">START</h2>
-              <h1 className="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">Free</h1>
+              <h1 className="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{plan[0].price}</h1>
               <p className="flex items-center text-gray-600 mb-2">
                 <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
@@ -59,7 +64,7 @@ function App() {
               <span className="bg-purple-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span>
               <h2 className="text-sm tracking-widest title-font mb-1 font-medium">PRO</h2>
               <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                <span>$38</span>
+                <span>{plan[1].price}</span>
                 <span className="text-lg ml-1 font-normal text-gray-500">/mo</span>
               </h1>
               <p className="flex items-center text-gray-600 mb-2">
@@ -102,7 +107,7 @@ function App() {
             <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
               <h2 className="text-sm tracking-widest title-font mb-1 font-medium">BUSINESS</h2>
               <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                <span>$56</span>
+                <span>{plan[2].price}</span>
                 <span className="text-lg ml-1 font-normal text-gray-500">/mo</span>
               </h1>
               <p className="flex items-center text-gray-600 mb-2">
@@ -152,7 +157,7 @@ function App() {
             <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
               <h2 className="text-sm tracking-widest title-font mb-1 font-medium">SPECIAL</h2>
               <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                <span>$72</span>
+                <span>{plan[3].price}</span>
                 <span className="text-lg ml-1 font-normal text-gray-500">/mo</span>
               </h1>
               <p className="flex items-center text-gray-600 mb-2">
---
+++ src/components/Plan.jsx
@@ -0,0 +1,18 @@
+export const monthlyPlan = [
+    {
+        id: 1,
+        price: 'Free',
+    },
+    {
+        id: 2,
+        price: '$38',
+    },
+    {
+        id: 3,
+        price: '$56',
+    },
+    {
+        id: 4,
+        price: '$72',
+    }
+] 
